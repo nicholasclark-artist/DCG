7bf297ce4c212cfeedcde80094c8cb58528d96e3
Nicholas Clark <nicholasclark.artist@gmail.com>
2016-01-24 18:29:34 -0700

    setup userconfig dir

---
+++ optional/userconfig/dcg/serverconfig.hpp
@@ -0,0 +1,9 @@
+/*
+Author: SENSEI
+
+Last modified: 10/29/2015
+
+Description: handles server side DCG settings
+
+Note: this file only has an effect on the server
+__________________________________________________________________*/
