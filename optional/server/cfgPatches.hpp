bbad40bf0fd9ce8b1321451a0df6fcbff7de7754
Nicholas Clark <nicholasclark.artist@gmail.com>
2016-01-24 18:21:46 -0700

    adding files to server addon

---
+++ optional/server/cfgPatches.hpp
@@ -0,0 +1,11 @@
+class CfgPatches {
+	class ADDON {
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = REQUIRED_VERSION;
+		requiredAddons[] = {"cba_common",QUOTE(MAIN_ADDON)};
+		author[] = {"SENSEI"};
+		authorUrl = URL;
+		VERSION_CONFIG;
+	};
+};
