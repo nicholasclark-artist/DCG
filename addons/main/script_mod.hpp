0d3e7eed9fe57bc0b668420a61e0247a4c5fb9ca
Nicholas Clark <nicholasclark.artist@gmail.com>
2016-01-30 02:17:12 -0700

    updated version

--- addons/main/script_mod.hpp
+++ addons/main/script_mod.hpp
@@ -3,11 +3,11 @@
 #define MAJOR 3
 #define MINOR 1
 #define PATCHLVL 0
-#define BUILD 2
+#define BUILD 3
 #define VERSION MAJOR.MINOR.PATCHLVL.BUILD
 #define VERSION_AR MAJOR,MINOR,PATCHLVL,BUILD
 #define VERSION_RELEASE false // set to true before releasing
 #define REQUIRED_VERSION 1.52
 #define TITLE "Dynamic Combat Generator"
 #define TITLESHORT "DCG"
-#define URL "https://forums.bistudio.com/topic/176544-coop-dynamic-combat-generator/"
+#define URL "https://github.com/nicholasclark-artist/dcg"
