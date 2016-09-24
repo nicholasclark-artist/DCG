7fcb08854b6ba53456cc3372614998a2dbe767f0
Nicholas Clark <nicholasclark.artist@gmail.com>
2016-09-24 00:13:05 -0500

    update macros

--- addons/approval/script_component.hpp
+++ addons/approval/script_component.hpp
@@ -4,11 +4,11 @@
 #include "\d\dcg\addons\main\script_macros.hpp"
 
 #define PVEH_HINT QGVAR(pveh_hint)
-#define HINT_CODE format ["%1 = player; publicVariableServer '%1'", PVEH_HINT]
+#define HINT_CODE QUOTE(SETMVAR(PVEH_HINT,player); publicVariableServer QUOTE(PVEH_HINT))
 #define HINT_KEYID QUOTE(DOUBLES(ADDON,hint))
 #define HINT_NAME "Check Approval in Region"
 
 #define PVEH_QUESTION QGVAR(pveh_question)
-#define QUESTION_CODE format ["%1 = player; publicVariableServer '%1'", PVEH_QUESTION]
+#define QUESTION_CODE QUOTE(SETMVAR(PVEH_QUESTION,player); publicVariableServer QUOTE(PVEH_QUESTION))
 #define QUESTION_KEYID QUOTE(DOUBLES(ADDON,question))
 #define QUESTION_NAME "Question Nearby Person"
