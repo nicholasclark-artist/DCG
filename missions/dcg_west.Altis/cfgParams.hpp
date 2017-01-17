b16a1892ea8128db6d5d030858960de777fce050
Nicholas <nicholasclark.artist@gmail.com>
2017-01-16 21:29:03 -0600

    update mission template

--- missions/dcg_west.Altis/cfgParams.hpp
+++ missions/dcg_west.Altis/cfgParams.hpp
@@ -2,59 +2,11 @@
     Types (SCALAR, BOOL, SIDE)
 */
 
-class dcg_main_debug {
-   title = "Debug Mode";
-   values[] = {0,1};
-   texts[] = {"Off", "On"};
-   default = 0;
-   dcg_setting = 1;
-   typeName = "SCALAR";
-};
-class dcg_main_loadData {
-   title = "Load Mission Data";
+/*class dcg_main_testSetting {
+   title = "";
    values[] = {0,1};
    texts[] = {"Off", "On"};
    default = 0;
    dcg_setting = 1;
    typeName = "BOOL";
-};
-class dcg_main_autoSave {
-   title = "Autosave Mission Data";
-   values[] = {0,1};
-   texts[] = {"Off", "On"};
-   default = 0;
-   dcg_setting = 1;
-   typeName = "BOOL";
-};
-class dcg_main_enemySide {
-   title = "Enemy Side";
-   values[] = {0,2};
-   texts[] = {"East", "Independent"};
-   default = 0;
-   dcg_setting = 1;
-   typeName = "SIDE";
-};
-class dcg_mission_disableCam {
-   title = "Disable Third Person Camera";
-   values[] = {0,1};
-   texts[] = {"Off", "On"};
-   default = 0;
-   dcg_setting = 1;
-   typeName = "BOOL";
-};
-class dcg_weather_season {
-   title = "Season";
-   values[] = {-1,0,1,2,3};
-   texts[] = {"Random","Summer","Fall","Winter","Spring"};
-   default = -1;
-   dcg_setting = 1;
-   typeName = "SCALAR";
-};
-class dcg_weather_time {
-   title = "Time of Day";
-   values[] = {-1,0,1,2,3};
-   texts[] = {"Random","Morning","Midday","Evening","Night"};
-   default = -1;
-   dcg_setting = 1;
-   typeName = "SCALAR";
-};
+};*/
