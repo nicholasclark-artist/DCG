5769ed33c458346ab2754a9c0c65b7e43c5d8965
Nicholas <nicholasclark.artist@gmail.com>
2017-03-08 00:46:10 -0600

    update range

--- addons/patrol/script_component.hpp
+++ addons/patrol/script_component.hpp
@@ -3,9 +3,9 @@
 #include "\d\dcg\addons\main\script_mod.hpp"
 
 // #define DEBUG_MODE_FULL
-// #define DISABLE_COMPILE_CACHE
+#define DISABLE_COMPILE_CACHE
 
 #include "\d\dcg\addons\main\script_macros.hpp"
 
 #define PATROL_MINRANGE 300
-#define PATROL_RANGE 1000
+#define PATROL_RANGE 700
