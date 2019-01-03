ae870877285e3f6cd11b5a059096a80b2ed6ed0b
Nicholas <nicholasclark.artist@gmail.com>
2019-01-02 21:11:57 -0600

    change to component specific classes

--- addons/main/DCG_3DEN.hpp
+++ addons/main/DCG_3DEN.hpp
@@ -1,18 +1,29 @@
+/*
+    https://community.bistudio.com/wiki/Eden_Editor:_Configuring_Attributes
+*/
+
+// menus
 #define MAIN_FOLDER GVARMAIN(folder)
-#define EXPORTFACTION_ITEM GVARMAIN(exportFaction)
+#define EXPORTFACTION_ITEM GVAR(exportFaction)
 #define EXPORTFACTION_TEXT "Export Faction List"
-#define EXPORTCOMP_ITEM GVARMAIN(exportComp)
+#define EXPORTCOMP_ITEM GVAR(exportComp)
 #define EXPORTCOMP_TEXT "Export Composition from Selection"
 
-#define COMP_ITEM GVARMAIN(comp)
+// attributes
+#define SAVE_ITEM GVAR(save)
+#define SAVE_TEXT "DCG - Save"
+#define SAVE_ENTITY GVAR(saveEntity)
+#define SAVE_ENTITY_TEXT "Save Entity"
+#define SAVE_ENTITY_TIP "Add entity to save system."
+#define COMP_ITEM GVAR(comp)
 #define COMP_TEXT "DCG - Composition"
-#define COMP_ANCHOR GVARMAIN(anchor)
+#define COMP_ANCHOR GVAR(anchor)
 #define COMP_ANCHOR_TEXT "Composition Anchor"
 #define COMP_ANCHOR_TIP "Enable to set object as composition anchor. Only one object in a composition should be the anchor."
-#define COMP_VECTORUP GVARMAIN(vectorUp)
+#define COMP_VECTORUP GVAR(vectorUp)
 #define COMP_VECTORUP_TEXT "Ignore Terrain Vector"
 #define COMP_VECTORUP_TIP ""
-#define COMP_SNAP GVARMAIN(snap)
+#define COMP_SNAP GVAR(snap)
 #define COMP_SNAP_TEXT "Snap Object to Terrain"
 #define COMP_SNAP_TIP ""
 
@@ -53,23 +64,12 @@ class Cfg3DEN {
 						tooltip = COMP_ANCHOR_TIP;
     					property = QUOTE(COMP_ANCHOR);
 						control = "Checkbox";
-						// Expression called when applying the attribute in Eden and at the scenario start
-						// The expression is called twice - first for data validation, and second for actual saving
-						// Entity is passed as _this, value is passed as _value
-						// %s is replaced by attribute config name. It can be used only once in the expression
-						// In MP scenario, the expression is called only on server.
 						expression = "";
-						// Expression called when custom property is undefined yet (i.e., when setting the attribute for the first time)
-						// Entity is passed as _this
-						// Returned value is the default value
-						// Used when no value is returned, or when it's of other type than NUMBER, STRING or ARRAY
-						// Custom attributes of logic entities (e.g., modules) are saved always, even when they have default value
 						defaultValue = "false";
-						//--- Optional properties
-						unique = 0; // When 1, only one entity of the type can have the value in the mission (used for example for variable names or player control)
-						// validate = "number"; // Validate the value before saving. Can be "none", "expression", "condition", "number" or "variable"
-						// condition = "objectSimulated"; // Condition for attribute to appear (see the table below)
-						// typeName = "NUMBER"; // Defines data type of saved value, can be STRING, NUMBER or BOOL. Used only when control is "Combo", "Edit" or their variants
+						unique = 0;
+						// validate = "number";
+						condition = "1 - (objectAgent + logicModule + objectBrain)";
+						// typeName = "NUMBER";
 					};
                     class COMP_VECTORUP {
                         displayName = COMP_VECTORUP_TEXT;
@@ -80,7 +80,7 @@ class Cfg3DEN {
                         defaultValue = "false";
                         unique = 0;
                         // validate = "number";
-                        // condition = "objectSimulated";
+                        condition = "1 - (objectAgent + logicModule + objectBrain)";
                         // typeName = "NUMBER";
                     };
                     class COMP_SNAP {
@@ -92,9 +92,27 @@ class Cfg3DEN {
                         defaultValue = "true";
                         unique = 0;
                         // validate = "number";
-                        // condition = "objectSimulated";
+                        condition = "1 - (objectAgent + logicModule + objectBrain)";
                         // typeName = "NUMBER";
                     };
+				};
+			};
+			class SAVE_ITEM {
+				displayName = SAVE_TEXT;
+				collapsed = 1;
+				class Attributes {
+					class SAVE_ENTITY {
+						displayName = SAVE_ENTITY_TEXT;
+						tooltip = SAVE_ENTITY_TIP;
+    					property = QUOTE(SAVE_ENTITY);
+						control = "Checkbox";
+						expression = QUOTE(_this setVariable [ARR_3(QQGVAR(saveEntity),_value,false)]);
+						defaultValue = "false";
+						unique = 0;
+						// validate = "number";
+						condition = "1 - (objectAgent + logicModule)";
+						// typeName = "NUMBER";
+					};
 				};
 			};
 		};
--- addons/safezone/DCG_3DEN.hpp
+++ addons/safezone/DCG_3DEN.hpp
@@ -1,6 +1,10 @@
+/*
+    https://community.bistudio.com/wiki/Eden_Editor:_Configuring_Attributes
+*/
+
 #define SAFEZONE_ITEM ADDON
 #define SAFEZONE_TEXT "DCG - Safezone"
-#define SAFEZONE_CREATE DOUBLES(ADDON,create)
+#define SAFEZONE_CREATE GVAR(create)
 #define SAFEZONE_CREATE_TEXT "Set as Safezone"
 #define SAFEZONE_CREATE_TIP "Enable to set as safezone."
 
@@ -16,23 +20,12 @@ class Cfg3DEN {
 						tooltip = SAFEZONE_CREATE_TIP;
     					property = QUOTE(SAFEZONE_CREATE);
 						control = "Checkbox";
-						// Expression called when applying the attribute in Eden and at the scenario start
-						// The expression is called twice - first for data validation, and second for actual saving
-						// Entity is passed as _this, value is passed as _value
-						// %s is replaced by attribute config name. It can be used only once in the expression
-						// In MP scenario, the expression is called only on server.
-						expression = QUOTE(call FUNC(init));
-						// Expression called when custom property is undefined yet (i.e., when setting the attribute for the first time)
-						// Entity is passed as _this
-						// Returned value is the default value
-						// Used when no value is returned, or when it's of other type than NUMBER, STRING or ARRAY
-						// Custom attributes of logic entities (e.g., modules) are saved always, even when they have default value
+						expression = QUOTE([_this] call FUNC(init));
 						defaultValue = "false";
-						//--- Optional properties
-						unique = 0; // When 1, only one entity of the type can have the value in the mission (used for example for variable names or player control)
-						// validate = "number"; // Validate the value before saving. Can be "none", "expression", "condition", "number" or "variable"
-						// condition = "objectSimulated"; // Condition for attribute to appear (see the table below)
-						// typeName = "NUMBER"; // Defines data type of saved value, can be STRING, NUMBER or BOOL. Used only when control is "Combo", "Edit" or their variants
+						unique = 0;
+						// validate = "number";
+						// condition = "objectSimulated";
+						// typeName = "NUMBER";
 					};
 				};
 			};
