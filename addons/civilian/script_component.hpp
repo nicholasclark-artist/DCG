2a6d58b9a97003654962c7375c73ad34b84e680b
Nicholas <nicholasclark.artist@gmail.com>
2019-01-09 22:07:55 -0600

    update macros

--- addons/civilian/script_component.hpp
+++ addons/civilian/script_component.hpp
@@ -9,9 +9,5 @@
 #include "\d\dcg\addons\main\script_macros.hpp"
 
 #define CIV_HANDLER_DELAY 10
-#define CIV_ZDIST 50
+#define CIV_ZDIST 200
 #define CIV_LOCATION_ID(NAME) ([QUOTE(ADDON),NAME] joinString "_")
-
-#define CIV_VILLAGE_COUNT 5
-#define CIV_CITY_COUNT 10
-#define CIV_CAPITAL_COUNT 20
