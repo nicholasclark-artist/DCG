0c7484d36714d22a77090d952fb3b08fd9ecf0b9
Nicholas <nicholasclark.artist@gmail.com>
2020-09-22 00:16:08 -0500

    improve unit spawns

--- addons/garrison/script_component.hpp
+++ addons/garrison/script_component.hpp
@@ -12,25 +12,17 @@
 
 #define AO_COUNT_P1 2 // phase 1
 #define AO_COUNT_P2 1 // phase 2
-#define GAR_COUNT 1
 #define OP_COUNT 2
-#define TASK_COUNT 1
 
-#define PAT_GRPSIZE 4 // number of units in group
-#define PAT_SPACING 750 // distance in meters between patrol groups
-#define PAT_INF_GRPLIMIT 8 // max number of patrol groups per area
-#define PAT_SHIP_GRPLIMIT 2 // max number of patrol groups per area
-#define PAT_SHIP_CARGO 1 // number of units to cargo
+#define AO_INF_GRPSIZE 4 // number of units in group
+#define AO_INF_GRPSPACING 750 // distance in meters between patrol groups
+#define AO_INF_GRPLIMIT 8 // max number of patrol groups per area
+#define AO_SHIP_GRPLIMIT 2 // max number of patrol groups per area
+#define AO_SHIP_CARGO 1 // number of units to cargo
 
 #define DYNPAT_RANGE 5000 // max distance from AO to consider dynamic patrols
 #define DYNPAT_SPAWNRANGE 500 // max distance from players to spawn dynamic patrol
 #define DYNPAT_GRPLIMIT 4 // max number of dynamic patrol groups
-
-#define GAR_SCORE 10
-#define OP_SCORE 5
-#define TASK_SCORE 5
-#define COMM_SCORE 2.5
-#define ROADBLOCK_SCORE 1
 
 #define INTEL_ITEMS ["intel_file1_f","intel_file2_f","land_document_01_f","land_laptop_unfolded_f","land_laptop_03_black_f","land_laptop_03_sand_f","land_laptop_03_olive_f"]
 #define INTEL_SURFACES ["land_campingtable_f","land_campingtable_small_f","land_campingtable_small_white_f","land_campingtable_white_f","land_tableplastic_01_f","land_woodentable_large_f","land_woodentable_small_f","officetable_01_new_f","officetable_01_old_f","land_portabledesk_01_olive_f"]
